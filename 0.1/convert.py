86dbb802ec552613c44047ec0d8deef7799acaf4
tonkotsurobot <38755247+tonkotsurobot@users.noreply.github.com>
2018-05-14 22:56:00 +1000

    Update convert.py

--- 0.1/convert.py
+++ 0.1/convert.py
@@ -4,4 +4,4 @@ command1 = "ffmpeg -stats -i "
 command2 = "-c:v copy -acodec ac3 -b:a 640k -ac 6 -c:s copy -f mp4"
 for filename in os.listdir(sys.argv[1]):
   subprocess.call(['ffmpeg', '-stats', '-i', sys.argv[1] + filename, '-c:v', 'copy', '-acodec', 'ac3', '-b:a', '640k', '-ac', '6', '-c:s', 'copy', '-f', 'mp4', sys.argv[2] + filename])
-  subprocess.call(['/bin/rm', sys.argv[1] + filename])
+  #subprocess.call(['/bin/rm', sys.argv[1] + filename])
